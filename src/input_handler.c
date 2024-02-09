394a9ab212737d599dba17b5849f69897601b8a8
selgas <s.elgas@techsmith.com>
2024-02-09 15:35:43 -0500

    Arrow keys change the actor's velocity

--- src/input_handler.c
+++ src/input_handler.c
@@ -1,13 +1,19 @@
 #include "game.h"
 #include "input_state.h"
+#include "actor.h"
+#include "entity.h"
+
+static void qInputHandler_HandleMapInput( qGame_t* game );
 
 void qInputHandler_HandleInput( qGame_t* game )
 {
    if ( qInputState_WasKeyPressed( game->inputState, sfKeyEscape ) )
    {
       qGame_Close( game );
+      return;
    }
-   else if ( qInputState_WasKeyPressed( game->inputState, sfKeyF8 ) )
+
+   if ( qInputState_WasKeyPressed( game->inputState, sfKeyF8 ) )
    {
       TOGGLE_BOOL( game->showDiagnostics );
 
@@ -18,6 +24,83 @@ void qInputHandler_HandleInput( qGame_t* game )
       else
       {
          qGame_ShowDebugMessage( game, STR_DEBUG_DIAGNOSTICSOFF );
+      }
+   }
+
+   qInputHandler_HandleMapInput( game );
+}
+
+static void qInputHandler_HandleMapInput( qGame_t* game )
+{
+   qActor_t* actor = game->controllingActor;
+   qEntity_t* entity = actor->entity;
+
+   sfBool leftIsDown = qInputState_IsKeyDown( sfKeyLeft );
+   sfBool upIsDown = qInputState_IsKeyDown( sfKeyUp );
+   sfBool rightIsDown = qInputState_IsKeyDown( sfKeyRight );
+   sfBool downIsDown = qInputState_IsKeyDown( sfKeyDown );
+
+   if ( leftIsDown && !rightIsDown )
+   {
+      entity->velocity.x = -entity->maxVelocity;
+
+      if ( upIsDown || downIsDown )
+      {
+         entity->velocity.x *= 0.707f;
+      }
+
+      if ( !( upIsDown && entity->direction == qDirection_Up ) &&
+           !( downIsDown && entity->direction == qDirection_Down ) )
+      {
+         entity->direction = qDirection_Left;
+      }
+   }
+
+   if ( upIsDown && !downIsDown )
+   {
+      entity->velocity.y = -entity->maxVelocity;
+
+      if ( leftIsDown || rightIsDown )
+      {
+         entity->velocity.y *= 0.707f;
+      }
+
+      if ( !( leftIsDown && entity->direction == qDirection_Left ) &&
+           !( rightIsDown && entity->direction == qDirection_Right ) )
+      {
+         entity->direction = qDirection_Up;
+      }
+   }
+
+   if ( rightIsDown && !leftIsDown )
+   {
+      entity->velocity.x = entity->maxVelocity;
+
+      if ( upIsDown || downIsDown )
+      {
+         entity->velocity.x *= 0.707f;
+      }
+
+      if ( !( upIsDown && entity->direction == qDirection_Up ) &&
+           !( downIsDown && entity->direction == qDirection_Down ) )
+      {
+         entity->direction = qDirection_Right;
+      }
+   }
+
+   if ( downIsDown && !upIsDown )
+   {
+      entity->velocity.y = entity->maxVelocity;
+
+      if ( leftIsDown || rightIsDown )
+      {
+         entity->velocity.y *= 0.707f;
+      }
+
+      if ( !( leftIsDown && entity->direction == qDirection_Left ) &&
+           !( rightIsDown && entity->direction == qDirection_Right ) )
+      {
+         entity->direction = qDirection_Down;
       }
    }
 }
